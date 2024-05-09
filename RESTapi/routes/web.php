8ea5656e9c254c68c5eda1f04b8b4b5557b7592d
Rilr20 <nrrl99@gmail.com>
2024-05-09 16:37:12 +0200

    api routes

--- RESTapi/routes/web.php
+++ RESTapi/routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/cart/items', 'CartController@AddItems');
-Route::get('/cart', 'CartController@GetCart');
-Route::put('/cart/items/{id}', "CartController@EditItem");
-Route::delete('cart/items/{id}', "CartController@DeleteItem");
+Route::post('/cart/items', [CartController::class, 'AddItems']);
+Route::get('/cart', [CartController::class, 'GetCart']);
+Route::put('/cart/items/{id}', [CartController::class, 'EditItem']);
+Route::delete('cart/items/{id}', [CartController::class, 'DeleteItem']);
